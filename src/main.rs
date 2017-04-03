6ccbb5da4234b0c37241d0a68ac52e1a5be1f7b0
William Alexander <willwalexander@icloud.com>
2017-04-03 03:27:46 +0000

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -16,27 +16,25 @@ mod crypt;
 
 fn salt_replace(c: char) -> char {
     match c {
-        '/' |
-        '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' |
-        'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J' | 'K' | 'L' | 'M' |
-        'N' | 'O' | 'P' | 'Q' | 'R' | 'S' | 'T' | 'U' | 'V' | 'W' | 'X' | 'Y' | 'Z' |
-        'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' |
-        'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z'
-             => c,
-        ':'  => 'A',
-        ';'  => 'B',
-        '<'  => 'C',
-        '='  => 'D',
-        '>'  => 'E',
-        '?'  => 'F',
-        '@'  => 'G',
-        '['  => 'a',
+        '/' | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | 'A' | 'B' | 'C' |
+        'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J' | 'K' | 'L' | 'M' | 'N' | 'O' | 'P' | 'Q' |
+        'R' | 'S' | 'T' | 'U' | 'V' | 'W' | 'X' | 'Y' | 'Z' | 'a' | 'b' | 'c' | 'd' | 'e' |
+        'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' |
+        't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z' => c,
+        ':' => 'A',
+        ';' => 'B',
+        '<' => 'C',
+        '=' => 'D',
+        '>' => 'E',
+        '?' => 'F',
+        '@' => 'G',
+        '[' => 'a',
         '\\' => 'b',
-        ']'  => 'c',
-        '^'  => 'd',
-        '_'  => 'e',
-        '`'  => 'f',
-        _    => '.',
+        ']' => 'c',
+        '^' => 'd',
+        '_' => 'e',
+        '`' => 'f',
+        _ => '.',
     }
 }
 
@@ -51,42 +49,43 @@ fn main() {
     let pats: Vec<String> = env::args().collect();
     let now = Instant::now();
 
-    let threads: Vec<_> = (0..procs).map(|_| {
-        let done = done.clone();
-        let pats = pats.clone();
+    let threads: Vec<_> = (0..procs)
+        .map(|_| {
+            let done = done.clone();
+            let pats = pats.clone();
 
-        thread::spawn(move || {
-            let mut n: u64 = 0;
+            thread::spawn(move || {
+                let mut n: u64 = 0;
 
-            while !done.load(Ordering::Relaxed) {
-                let pass: String = rand::thread_rng()
-                    .gen_ascii_chars()
-                    .take(9)
-                    .collect();
+                while !done.load(Ordering::Relaxed) {
+                    let pass: String = rand::thread_rng().gen_ascii_chars().take(9).collect();
 
-                let salt: String = pass.chars()
-                    .chain("H.".chars())
-                    .skip(1)
-                    .take(2)
-                    .map(salt_replace)
-                    .collect();
+                    let salt: String = pass.chars()
+                        .chain("H.".chars())
+                        .skip(1)
+                        .take(2)
+                        .map(salt_replace)
+                        .collect();
 
-                if let Some(t) = crypt::crypt(&pass, &salt) {
-                    if pats.iter().any(|p| t.contains(p.as_str())) {
-                        println!("#{} => {}", pass, t);
+                    if let Some(t) = crypt::crypt(&pass, &salt) {
+                        if pats.iter().any(|p| t.contains(p.as_str())) {
+                            println!("#{} => {}", pass, t);
+                        }
                     }
+
+                    n += 1;
                 }
 
-                n += 1;
-            }
-
-            n
+                n
+            })
         })
-    }).collect();
+        .collect();
 
     io::stdin().bytes().next();
     done.store(true, Ordering::Relaxed);
-    let n = threads.into_iter().fold(0, |acc, t| acc + t.join().unwrap());
+    let n = threads
+        .into_iter()
+        .fold(0, |acc, t| acc + t.join().unwrap());
     let tps = n / now.elapsed().as_secs();
     println!("{} tripcodes/second", tps);
 }
